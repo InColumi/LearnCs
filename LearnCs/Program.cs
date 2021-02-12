f0069b3b685dbf84b8758869865b3a0b3fd24998
Maxim Panov <max77648@gmail.com>
2021-02-12 06:33:25 +0300

    Поиск фото в яндексе

--- LearnCs/Program.cs
+++ LearnCs/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,32 @@ namespace LearnCs
     {
         static void Main(string[] args)
         {
+            string startSearch = "https://yandex.ua/images/search?text=";
+            string nameCar = Console.ReadLine();
+            StringBuilder builder = new StringBuilder();
+            string[] nameCarSplit = nameCar.Split(' ');
+            for (int i = 0; i < nameCarSplit.Length - 1; i++)
+            {
+                builder.Append(nameCarSplit[i]);
+                builder.Append("%20");
+            }
+            builder.Append(nameCarSplit[nameCarSplit.Length - 1]);
+            nameCar = builder.ToString();
+            builder.Clear();
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                while (reader.EndOfStream == false)
+                {
+                    builder.Append(startSearch);
+                    builder.Append(reader.ReadLine());
+                    builder.Append(nameCar);
+                    Console.WriteLine(builder.ToString());
+                    Process.Start(builder.ToString());
+                    builder.Clear();
+                }
+            }
+            Console.WriteLine("Search complited.");
+            Console.ReadKey();
         }
     }
 }
