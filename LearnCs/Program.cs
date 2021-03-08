3ce7c05b3db1133531f49a9d4d572d227e76e048
Maxim Panov <max77648@gmail.com>
2021-03-08 07:40:43 +0300

    Добавил сортировку при поиске в яндексе

--- LearnCs/Program.cs
+++ LearnCs/Program.cs
@@ -12,32 +12,105 @@ namespace LearnCs
     {
         static void Main(string[] args)
         {
-            string startSearch = "https://yandex.ua/images/search?text=";
+
+            Console.WriteLine("1) Create folders.");
+            Console.WriteLine("2) Search in yandex.");
+            Console.WriteLine("Please enter number of task:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter car name: ");
             string nameCar = Console.ReadLine();
-            StringBuilder builder = new StringBuilder();
-            string[] nameCarSplit = nameCar.Split(' ');
-            for (int i = 0; i < nameCarSplit.Length - 1; i++)
+            DoTaskBy(number, nameCar);
+
+            Console.ReadKey();
+        }
+
+        private static void DoTaskBy(int numberOfTask, string nameCar)
+        {
+            switch (numberOfTask)
             {
-                builder.Append(nameCarSplit[i]);
-                builder.Append("%20");
+                case 1:
+                    DoTask1(nameCar);
+                    break;
+                case 2:
+                    DoTask2(nameCar);
+                    break;
+                default:
+                    Console.WriteLine($"Task with {numberOfTask} is not exist!");
+                    break;
             }
-            builder.Append(nameCarSplit[nameCarSplit.Length - 1]);
-            nameCar = builder.ToString();
-            builder.Clear();
+        }
+
+        private static void DoTask1(string nameCar)
+        {
+            StringBuilder builder = new StringBuilder();
+            string builderInString;
             using (StreamReader reader = new StreamReader("input.txt"))
             {
                 while (reader.EndOfStream == false)
                 {
-                    builder.Append(startSearch);
+                    builder.Append("md \"");
                     builder.Append(reader.ReadLine());
+                    builder.Append(" ");
                     builder.Append(nameCar);
-                    Console.WriteLine(builder.ToString());
-                    Process.Start(builder.ToString());
+                    builder.Append('"');
+                    builderInString = builder.ToString();
+                    Console.WriteLine(builderInString);
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = $"cmd",
+                        Arguments = $"/c {builderInString}",
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    });
                     builder.Clear();
                 }
             }
+            Console.WriteLine("Folders created.");
+        }
+
+        private static void DoTask2(string nameCar)
+        {
+            string startSearch = "https://yandex.ua/images/search?text=";
+
+            nameCar = GetNewFormat(nameCar, ' ', "%20");
+
+            StringBuilder builder = new StringBuilder();
+            string builderInString;
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                while (reader.EndOfStream == false)
+                {
+                    lines.Add(reader.ReadLine());
+                }                
+            }
+
+            lines.Sort();
+            foreach (var line in lines)
+            {
+                builder.Append(startSearch);
+                builder.Append(GetNewFormat(line, ' ', "%20"));
+                builder.Append("%20");
+                builder.Append(nameCar);
+                builderInString = builder.ToString();
+                Console.WriteLine(builderInString);
+                Process.Start(builderInString);
+                builder.Clear();
+            }
             Console.WriteLine("Search complited.");
-            Console.ReadKey();
+        }
+
+        private static string GetNewFormat(string line, char olsSymbol, string newSymbol)
+        {
+            StringBuilder builder = new StringBuilder();
+            string[] words = line.Split(olsSymbol);
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                builder.Append(words[i]);
+                builder.Append(newSymbol);
+            }
+            builder.Append(words[words.Length - 1]);
+            return builder.ToString();
         }
     }
 }
